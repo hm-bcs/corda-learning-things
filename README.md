78c8ee448d21494a5113450da34882ed300b9e95
hm-bcs <34492936+hm-bcs@users.noreply.github.com>
2018-01-15 12:16:41 +1100

    Added in pretty contract tests

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # corda-learning-things
 Cool repo.
 
-`gradle deployNodes`
+`gradle deployNodes`<br>
+`gradle test integrationTest smokeTest` with any combination of `test`, `integrationTest` and `smokeTest`
 
 Issue from Corporation to Bank of Harrison<br>
 `start HarrisonIssueRequestFlow harrisonValue: 84, otherParty: "O=BankOfHarrison,L=Tokyo,C=JP"`
 
 Check vault on Corporation<br>
 `run vaultQuery contractStateType: com.template.HarrisonState`
+
+
+### Learning
+https://github.com/corda/cordapp-example/blob/release-V2/kotlin-source/src/test/kotlin/com/example/contract/IOUContractTests.kt
--- cordapp/src/test/kotlin/com/template/ContractTests.kt
+++ cordapp/src/test/kotlin/com/template/ContractTests.kt
@@ -1,7 +1,6 @@
 package com.template
 
-import net.corda.testing.setCordappPackages
-import net.corda.testing.unsetCordappPackages
+import net.corda.testing.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -19,5 +18,99 @@ class ContractTests {
     }
 
     @Test
-    fun `dummy test`() = Unit
+    fun emptyLedger() {
+        ledger {  }
+    }
+
+    @Test
+    fun `Issuance Transaction must have a create command`() {
+        val money = 5
+        ledger {
+            // transactions can hold input, output, command, and attachment
+            transaction {
+                //                                                    value    owner     issuer
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP )}
+                fails()
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) {HarrisonIssueContract.Create()}
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `Issuance Transaction must have no inputs`() {
+        val money = 5
+        ledger {
+            transaction {
+                input(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("No inputs should be consumed")
+            }
+        }
+    }
+
+    @Test
+    fun `Issuance Transaction must have one output`() {
+        val money = 5
+        ledger {
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP) }
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Only one output")
+            }
+        }
+    }
+
+    @Test
+    fun `Issuance Transaction value must not be negative`() {
+        val money = -5
+        ledger {
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Non negative value")
+            }
+        }
+    }
+
+    @Test
+    fun `Issuance Transaction requires two signers`() {
+        val money = 5
+        ledger {
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(MINI_CORP_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Must be two signers")
+            }
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(MEGA_CORP_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Must be two signers")
+            }
+        }
+    }
+
+    @Test
+    fun `Issuance Transaction requires issuer and owner to be signers`() {
+        val money = 5
+        ledger {
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(MINI_CORP_PUBKEY, ALICE_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Signers must be issuer and receiver")
+            }
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(ALICE_PUBKEY, MEGA_CORP_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Signers must be issuer and receiver")
+            }
+            transaction {
+                output(HARRISON_ISSUANCE_CONTRACT_ID) { HarrisonState(money, MINI_CORP, MEGA_CORP)}
+                command(ALICE_PUBKEY, BOB_PUBKEY) { HarrisonIssueContract.Create() }
+                `fails with`("Signers must be issuer and receiver")
+            }
+        }
+    }
 }
