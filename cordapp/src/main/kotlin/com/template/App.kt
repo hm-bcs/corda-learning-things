0529a424471092e9cec5dc14c8749bf3a09a5902
hm-bcs <34492936+hm-bcs@users.noreply.github.com>
2018-01-19 12:11:55 +1100

    PUT Request added. Gradle wrapper added.

--- cordapp/src/main/kotlin/com/template/App.kt
+++ cordapp/src/main/kotlin/com/template/App.kt
@@ -5,32 +5,91 @@ import net.corda.core.contracts.Command
 import net.corda.core.contracts.StateAndContract
 import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import net.corda.core.internal.randomOrNull
 import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.messaging.startTrackedFlow
+import net.corda.core.messaging.vaultQueryBy
 import net.corda.core.serialization.SerializationWhitelist
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
+import net.corda.core.utilities.getOrThrow
 import net.corda.webserver.services.WebServerPluginRegistry
+import sun.security.provider.certpath.OCSPResponse
 import java.util.function.Function
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.print.DocFlavor
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
 // *****************
 // * API Endpoints *
 // *****************
+val SERVICE_NAMES = listOf("Controller", "Network Map Service")
+
 @Path("template")
 class TemplateApi(val rpcOps: CordaRPCOps) {
+    private val myLegalName: CordaX500Name = rpcOps.nodeInfo().legalIdentities.first().name
     // Accessible at /api/template/templateGetEndpoint.
     @GET
     @Path("templateGetEndpoint")
     @Produces(MediaType.APPLICATION_JSON)
     fun templateGetEndpoint(): Response {
         return Response.ok("Template GET endpoint.").build()
+    }
+
+    // /api/template/getPeers
+    @GET
+    @Path("getPeers")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getPeers(): Map<String, List<CordaX500Name>> {
+        val nodeInfo = rpcOps.networkMapSnapshot()
+        // all peers which is not current organisation, or Controller/Network Map Service
+        return mapOf("peers" to nodeInfo
+                .map { it.legalIdentities.first().name }
+                .filter { it.organisation !in (SERVICE_NAMES + myLegalName.organisation) })
+    }
+
+    @GET
+    @Path("me")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun whoami() = mapOf("me" to myLegalName)
+
+    @GET
+    @Path("Harrisons")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getHarrisons() = rpcOps.vaultQueryBy<HarrisonState>().states
+
+    @PUT
+    @Path("issue-harrison")
+    fun issueHarrison(@QueryParam("value") value : Int,
+                      @QueryParam("bank") bankString : String) : Response {
+
+        val bankParty: CordaX500Name = CordaX500Name.parse(bankString)
+        // Checks
+        if(value < 0) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Value must not be negative.\n").build()
+        }
+        if(value % 42 != 0) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Value must be divisible by 42.\n").build()
+        }
+        val bank = rpcOps.wellKnownPartyFromX500Name(bankParty) ?:
+                return Response.status(Response.Status.BAD_REQUEST).entity("Party named $bankParty cannot be found.\n").build()
+
+        //
+        return try {
+            val flowHandle = rpcOps.startTrackedFlow(::HarrisonIssueRequestFlow, value, bank)
+            flowHandle.progress.subscribe{ println(">> $it") }
+
+            val result = flowHandle.returnValue.getOrThrow()
+
+            Response.status(Response.Status.CREATED).entity("Transaction id ${result.id} committed to ledger.\n").build()
+        } catch (ex: Throwable) {
+            // log me
+            Response.status(Response.Status.BAD_REQUEST).entity(ex.message!!).build()
+        }
     }
 }
 
